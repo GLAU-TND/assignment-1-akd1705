108facc1d9830079f05f0d610a2ae977496dc827
Aditya Kumar Dubey <43343014+akd1705@users.noreply.github.com>
2020-02-24 21:59:21 +0530

    Final update.commit...........

--- ContactApp/src/main/Main.java
+++ ContactApp/src/main/Main.java
@@ -15,7 +15,7 @@ public class Main {
         String emailOfContact = "";
         boolean ctr;
         while (ctr = true) {
-            System.out.println("Welcome to Shivam's Contact List App");
+            System.out.println("Welcome to Aditya's PhoneBook App");
             System.out.println("Press 1 to add a new contact\n" + "Press 2 to view all contacts\n" +
                     "Press 3 to search for a contact\n" + "Press 4 to delete a contact\n" + "Press 5 to exit program ");
 
